2b013d25068fa5d7c1aeaed8290a04802877b440
Yurii Roskydko <yrafisher1@spaces.ru>
2021-09-14 22:27:12 +0300

    show current version

--- js/about.js
+++ js/about.js
@@ -2,6 +2,9 @@ let payPal = document.getElementsByClassName("paypal")[0];
 let donationAlerts = document.getElementsByClassName("donationalerts")[0];
 let title = document.getElementsByClassName("title")[0];
 let language = navigator.language;
+let manifestData = chrome.runtime.getManifest();
+let titleAbout = document.getElementsByClassName("title-about")[0];
+
 
 payPal.onclick = () => {
     window.open("https://www.paypal.com/paypalme2/NightSoftware");
@@ -12,3 +15,4 @@ donationAlerts.onclick = () => {
 if (language == "ru") {
     title.innerHTML = chrome.i18n.getMessage("title");
 }
+titleAbout.innerHTML += " " + manifestData.version;
